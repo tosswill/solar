e7f6d73258241e0b9eaa5f49996c3eb6e47d1d73
Chris Tosswill <ctosswill@engineyard.com>
2011-06-27 12:32:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solar::Application.config.secret_token = 'b6f41a293009719cb88640d604aee7249e0b0c2e0323f2d8da1aa90e3fd8f382994066b09e0252a40b0a0b5591448b3097a4cba60dd4c0674a94a3ec3a3378a4'
